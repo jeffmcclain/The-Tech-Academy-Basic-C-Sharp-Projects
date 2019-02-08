c6d5630d336fc61e1fba55c0292de9ab49281cd2
Jeff <jmcclain47@gmail.com>
2019-02-07 17:20:39 -0800

    Add to repository

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main()
+    {
+
+        //List<int> intList = new List<int>();
+        //intList.Add(4); // add to the list
+        //intList.Add(10);
+        //intList.Remove(10); // remove from the list
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello"); // add to the list
+        stringList.Add("Jefe");
+        stringList.Remove("Jefe"); // remove from the list
+
+        Console.WriteLine(stringList[0]);
+        Console.ReadLine();
+
+        // a 'byte array' can be useful for very large arrays
+        byte[] byteArray = new byte[5000];
+
+
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //// you can accomplish the same as above with less code
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //// again you can accomplish this with even less code
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //// this will change the value located at index 5 of this array
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
+    }
+}
+
---
+++ step_100/step_100/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // create string array and assign some values
+            string[] stringArray = { "Apple", "Orange", "Mango", "Pineapple", "Durian", "Banana" };
+
+            // prompt user to enter a number 0-5 to access the corresponding index of the array
+            Console.WriteLine("\nWelcome to the mystery fruit machine. Please enter a selection (0-5).");
+            int choiceFruit = Convert.ToInt16(Console.ReadLine());   // convert user input from string to an integer
+
+            // check input and notify user if they chose an index that doesn't exist
+            if (choiceFruit > 5)
+            {
+                Console.WriteLine("Your selection doesn't exist!");
+            }
+            else
+            {
+                // print to console the index value associated with user's input
+                Console.WriteLine("Here's your " + stringArray[choiceFruit]);
+            }
+
+            // create integer array and assign values
+            int[] intArray = { 15, 2, 777, 1920312, 1, 123, };
+
+            // prompt user to enter a number 0-5 to access the corresponding index of the array
+            Console.WriteLine("\n\nEnter a number between 0-5 to receive your new lucky number...");
+            int choiceLuckyNum = Convert.ToInt16(Console.ReadLine());   // convert input from string to integer
+
+            // check input and notify user if they chose an index that doesn't exist
+            if (choiceLuckyNum > 5)
+            {
+                Console.WriteLine("Your selection does not exist!");
+            }
+            else
+            {
+                //print to console the index value associated with user's input
+                Console.WriteLine("Your new lucky number is: " + intArray[choiceLuckyNum]);
+            }
+
+            // create string list
+            List<string> stringList = new List<string>();
+            // add strings to list
+            stringList.Add("cat");
+            stringList.Add("newt");
+            stringList.Add("tiger");
+            stringList.Add("panda");
+            stringList.Add("llama");
+            stringList.Add("capybara");
+
+            // prompt user to enter a number selection
+            Console.WriteLine("\n\nNow time to receive your spirit animal. Enter a number 0-5.");
+            int choiceSpiritAnim = Convert.ToInt16(Console.ReadLine());
+
+            // check input and notify user if they chose an index that doesn't exist
+            if (choiceSpiritAnim> 5)
+            {
+                Console.WriteLine("Your selection does not exist!");
+                Console.ReadLine();
+                Environment.Exit(0);    // exit program
+            }
+            else
+            {
+                //print to console the index value associated with user's input
+                Console.WriteLine("Deep down, you are a " + stringList[choiceSpiritAnim]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
