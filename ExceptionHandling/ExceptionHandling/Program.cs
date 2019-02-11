45027264c478f3cf0f2973cb97cc23acc596ca3e
Jeff <jmcclain47@gmail.com>
2019-02-10 20:57:17 -0800

    Add to repository

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second numeber.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+        }
+        catch (FormatException ex) // catches input format errors
+        {
+            Console.WriteLine("Please type a whole number"); // prints custom error message
+            return; // stops execution of method
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero"); //This prints the custom error message
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message); // prints error message
+        }
+        finally // this executes regardless of a 'catch'
+        {
+            Console.ReadLine();
+        }
+        
+
+        Console.ReadLine();
+    }
+}
+
---
+++ step_106/step_106/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // create list numbers
+            List<int> listNums = new List<int>() { 16, 25, 30, 77, 100, 819, 2000, 11500, 15115, 2001234 };
+            
+            // create list to hold the quotient of each executed equation
+            List<int> quotientList = new List<int>();
+
+            try
+            {
+                // prompt user to enter a number
+                Console.WriteLine("Enter a number: ");
+                int userNum = Convert.ToInt32(Console.ReadLine()); // convert user input from string to integer
+
+                // iterate through list, dividing each index value by the user's selected number
+                foreach (int listNum in listNums)
+                {
+                    int quotient = listNum / userNum; // divide list index value by user input. Assign to new variable
+                    quotientList.Add(quotient);
+                    Console.WriteLine(listNum + " divided by " + userNum + " equals " + quotient);
+                }
+            }
+            catch (FormatException) // this catches non integer input and displays custom error message
+            {
+                Console.WriteLine("This program only accepts whole numbers.");
+                Console.ReadLine();
+                return;
+            }
+            catch (DivideByZeroException) // this catches user input of 0 and displays custom error message
+            {
+                Console.WriteLine("Zero is not a valid entry.");
+                Console.ReadLine();
+                return;
+            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            Console.WriteLine("\n\n...and the program continues on...");
+            Console.ReadLine();
+        }
+    }
+}
