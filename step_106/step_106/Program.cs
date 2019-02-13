810a23d8c5bbcf594cf7f47edf0ba2b85efe30b1
Jeff <jmcclain47@gmail.com>
2019-02-13 15:45:32 -0800

    Update Program.cs

--- step_106/step_106/Program.cs
+++ step_106/step_106/Program.cs
@@ -14,35 +14,43 @@ namespace step_106
             // create list of numbers
             List<int> listNums = new List<int>() { 16, 25, 30, 77, 100, 819, 2000, 11500, 15115, 2001234 };
 
-            try
-            {
-                // prompt user to enter a number
-                Console.WriteLine("Enter a number: ");
-                int userNum = Convert.ToInt32(Console.ReadLine()); // convert user input from string to integer
+            // declare bool to iterate the try/catch until the program input parameters are met
+            bool tryAgain = true;
 
-                // iterate through list, dividing each index value by the user's selected number
-                foreach (int listNum in listNums)
+            // while loop will execute until user input meets the program requirements
+            while (tryAgain)
+            { 
+                try
                 {
-                    int quotient = listNum / userNum; // divide list index value by user input. Assign to new variable
-                    Console.WriteLine("\n" + listNum + " divided by " + userNum + " equals " + quotient);
+                    // prompt user to enter a number
+                    Console.WriteLine("Enter a number: ");
+                    int userNum = Convert.ToInt32(Console.ReadLine()); // convert user input from string to integer
+
+                    // iterate through list, dividing each index value by the user's selected number
+                    foreach (int listNum in listNums)
+                    {
+                        int quotient = listNum / userNum; // divide list index value by user input. Assign to new variable
+                        Console.WriteLine("\n" + listNum + " divided by " + userNum + " equals " + quotient);
+
+                        // change value of boolean statement to end while loop
+                        tryAgain = false;
+                    }
                 }
+                catch (FormatException) // this catches non integer input and displays custom error message
+                {
+                    Console.WriteLine("This program only accepts whole numbers.");
+                    Console.ReadLine();
+                }
+                catch (DivideByZeroException) // this catches user input of 0 and displays custom error message
+                {
+                    Console.WriteLine("Zero is not a valid entry.");
+                    Console.ReadLine();
+                }
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex.Message);
+                //}
             }
-            catch (FormatException) // this catches non integer input and displays custom error message
-            {
-                Console.WriteLine("This program only accepts whole numbers.");
-                Console.ReadLine();
-                return;
-            }
-            catch (DivideByZeroException) // this catches user input of 0 and displays custom error message
-            {
-                Console.WriteLine("Zero is not a valid entry.");
-                Console.ReadLine();
-                return;
-            }
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.Message);
-            //}
 
             Console.WriteLine("\n\n...and the program continues on...");
             Console.ReadLine();
