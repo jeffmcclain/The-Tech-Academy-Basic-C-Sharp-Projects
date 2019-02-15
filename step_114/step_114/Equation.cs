9f266f06d583d556d24b858c0924d415447eaaaa
Jeff <jmcclain47@gmail.com>
2019-02-15 14:27:58 -0800

    Add to repository

---
+++ step_114/step_114/Equation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_114
+{
+    class Equation
+    {
+        // create method 'Math', pass in an integer from Main() and return an integer
+        public int Math(int wholeNum)
+        {
+            int answerInt = 1234 / wholeNum;
+            return answerInt; // return the stored integer
+        }
+
+        public int Math(decimal decimalNum)
+        {
+            int answerIntTwo = Convert.ToInt32(150 * decimalNum);
+            return answerIntTwo;
+        }
+
+        public int Math(string stringNum)
+        {
+            int num3 = Convert.ToInt32(stringNum);
+            int answerIntThree = 33 + num3;
+            return answerIntThree;
+        }
+    }
+}
---
+++ step_114/step_114/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // declare three variables and assign each a different datatype (integer, decimal and string)
+            int wholeNum = 47;
+            decimal decimalNum = 25.21m;
+            string stringNum = "87";
+
+
+            // instantiate the class 'Equation' 
+            Equation runMethod = new Equation();
+
+            // pass each dataype through the methods. Then print the resulting equations and conversions to console
+            runMethod.Math(wholeNum);
+            Console.WriteLine("1234 divided by " + wholeNum + " equals " + runMethod.Math(wholeNum));
+
+            runMethod.Math(decimalNum);
+            Console.WriteLine("\n150 multiplied by " + decimalNum + " equals " + runMethod.Math(decimalNum));
+
+            runMethod.Math(stringNum);
+            Console.WriteLine("\n\"33\" added to " + stringNum + " equals " + runMethod.Math(stringNum));
+
+
+            Console.ReadLine();
+        }
+    }
+}
