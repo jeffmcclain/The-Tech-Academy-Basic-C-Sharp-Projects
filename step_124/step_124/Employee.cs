2ae3031c91394e59da16a7250785dd85cfc5efb4
Jeff <jmcclain47@gmail.com>
2019-02-18 16:42:28 -0800

    Add to repository

---
+++ step_124/step_124/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_124
+{
+    public class Employee : Person
+    {
+        // implement 'SayName()' method with keyword 'override' in order to access within the abstract class 'Person'
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ step_124/step_124/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_124
+{
+    public abstract class Person
+    {
+        // declare properties 'firstName' and 'lastName'
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // create a method with keyword 'virtual' in order to implement in outside class
+        public virtual void SayName()
+        {
+            Console.WriteLine("Employee: " + firstName + " " + lastName);
+        }
+
+    }
+}
