6af2ea165738fa494c18e7ad4bd584780aa34da3
Jeff <jmcclain47@gmail.com>
2019-02-20 15:38:59 -0800

    Add to repository

---
+++ step_131/step_131/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_129
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+        
+
+        // implement 'SayName()' method with keyword 'override' in order to access within the abstract class 'Person'
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        
+        // overload '==' operator to compare two objects
+        public static bool operator ==(Employee<T> one, Employee<T> two)
+        {
+            if (one.Id == two.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee<T> one, Employee<T> two)
+        {
+            if (one.Id != two.Id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var employee = obj as Employee<T>;
+            return employee != null &&
+                   Id == employee.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
+        }
+
+        // loop through the object lists stored within the property 'Things' using generic data type 'T'
+        public void PrintThings (List<T> Things)
+        {
+            foreach (T Thing in Things)
+            {
+                Console.WriteLine(Thing);
+            }
+        }
+
+
+        public void Quit()
+        {
+            Console.WriteLine("ENTER to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ step_131/step_131/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate object and assign values to properties 'firstName' and 'lastName'
+            Employee<string> name = new Employee<string>();
+            name.firstName = "Sample";
+            name.lastName = "Student";
+            name.Id = 1;
+            name.Id = 2;
+
+            // instantiate 'Employee' object with data type string as it's generic parameter
+            Employee<string> stringThing = new Employee<string>();
+            stringThing.Things = new List<string>() { "thingOne", "ThingTwo", "thingThree", "thingFour" };
+            stringThing.PrintThings(stringThing.Things);
+
+            // instantiate 'Employee' object with data type int as it's generic parameter
+            Employee<int> intThing = new Employee<int>();
+            intThing.Things = new List<int>() { 1, 5, 9, 21, 923, 12340 };
+            intThing.PrintThings(intThing.Things);
+
+
+            // call method 'SayName()' on the object 'name'
+            name.SayName();
+
+            // using polymorphism instantiate object of type 'IQuittable' and call the 'Quit()' method
+            IQuittable quit = new Employee<IQuittable>();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
