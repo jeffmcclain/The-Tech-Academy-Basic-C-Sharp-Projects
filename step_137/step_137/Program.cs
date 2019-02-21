085c02cc057b1d2dc6fbdb880dff45e7d464e9a2
Jeff <jmcclain47@gmail.com>
2019-02-21 12:05:07 -0800

    Add to repository

---
+++ step_137/step_137/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an object from class 'Number' an assign data type decimal
+            Number number = new Number(); 
+            number.Amount = 12.358m; // assign a value 
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
