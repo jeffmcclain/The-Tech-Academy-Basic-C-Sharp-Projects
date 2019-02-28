d9689584d7f074db5dc986e929eb839393e3c9e0
Jeff <jmcclain47@gmail.com>
2019-02-27 17:53:46 -0800

    Add to repository

---
+++ step_162/step_162/MadLib.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_162
+{
+    public class MadLib
+    {
+
+        public MadLib(string name) : this(name, "Moon", "shooting", "star", "clear")
+        {
+        }
+
+        public MadLib(string name, string place, string verb, string noun, string adjective)
+        {
+            Name = name;
+            Place = place;
+            Verb = verb;
+            Noun = noun;
+            Adjective = adjective;
+        }
+
+        public string Name { get; set; }
+        public string Place { get; set; }
+        public string Noun { get; set; }
+        public string Verb { get; set; }
+        public string Adjective { get; set; }
+
+    }
+}
---
+++ step_162/step_162/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            // create a constant variable
+            const string greeting = "Welcome to MadLibs!";
+
+            Console.WriteLine(greeting);
+            Console.WriteLine("\nEnter your name...");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nEnter a place...");
+            string place = Console.ReadLine();
+            Console.WriteLine("\nEnter a verb...");
+            string verb = Console.ReadLine();
+            Console.WriteLine("\nEnter a noun...");
+            string noun = Console.ReadLine();
+            Console.WriteLine("\nEnter an adjective...");
+            string adjective = Console.ReadLine();
+
+            // create a variable using the keyword var
+            var madLib = new MadLib(name, place, verb, noun, adjective);
+
+            Console.WriteLine("\n\nHello, {0}! Welcome to {1}! A place where you " +
+                "can always find a {2} {3} and the skies are always {4}.", madLib.Name, madLib.Place,
+                madLib.Verb, madLib.Noun, madLib.Adjective);
+            Console.Read();
+
+        }
+    }
+}
